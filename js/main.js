c426901d0c03e6932c194922037fd194420726d3
VladSP <sp.vlad96@gmail.com>
2023-03-13 08:28:18 +0200

    First cimmit

---
+++ js/main.js
@@ -0,0 +1,3 @@
+let a = parseInt(prompt("Введите значение переменной a:"));
+let b = parseInt(prompt("Введите значение переменной b (больше a):"));
+let h = parseInt(prompt("Введите значение переменной h (больше 0):"));
