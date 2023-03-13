fd29e356f9ace90d5e525f00b28475fd6c243e47
VladSP <sp.vlad96@gmail.com>
2023-03-13 09:23:41 +0200

    Second cimmit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
-let a = parseInt(prompt("Введите значение переменной a:"));
-let b = parseInt(prompt("Введите значение переменной b (больше a):"));
-let h = parseInt(prompt("Введите значение переменной h (больше 0):"));
+let a = parseInt(prompt("Введіть значення a:"));
+let b = parseInt(prompt("Введіть значення b, більше за " + a + ":"));
+let h = parseInt(prompt("Введіть крок h (більший за 0):"));
+
+if (b <= a) {
+  alert("Значення b має бути більшим за a!");
+} else if (h <= 0) {
+  alert("Крок h має бути більшим за 0!");
+} else {
+  let sumOfFactorials = 0;
+  for (let i = a; i <= b; i += h) {
+    let factorial = 1;
+    for (let j = 1; j <= i; j++) {
+      factorial *= j;
+    }
+      sumOfFactorials += factorial;
+      console.log(sumOfFactorials);
+  }
+  alert("Сума факторіалів чисел від " + a + " до " + b + " з кроком " + h + " дорівнює " + sumOfFactorials);
+
+};
+
+
+
